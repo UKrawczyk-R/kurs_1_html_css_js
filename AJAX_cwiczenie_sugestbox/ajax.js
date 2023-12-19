9ad94f16e18b59a95d0a4b6f97e0701be5a70222
urszula.krawczyk_007@wp.pl <urszula.krawczyk_007@wp.pl>
2023-12-19 22:23:58 +0100

    add content

--- AJAX_cwiczenie_sugestbox/ajax.js
+++ AJAX_cwiczenie_sugestbox/ajax.js
@@ -1,10 +1,12 @@
 window.onload = init;
-XMLMainElement = null;
+ var XMLMainElement = null;
 function init()
     {
      document.getElementById("suggestBoxField").style.left = document.getElementById("wojewodztwo").offsetLeft + "px";
-     document.getElementById("suggestBoxField").style.top = document.getElementById("wojewodztwo").offsetTop + document.getElementById("wojewodztwo").offsetHeight + "px";
+     document.getElementById("suggestBoxField").style.top = document.getElementById("wojewodztwo").offsetTop 
+     + document.getElementById("wojewodztwo").offsetHeight + "px";
     
+     document.getElementById("wojewodztwo").onkeyup = showBox;
      suggestBox();
     }
 
@@ -28,7 +30,6 @@ function suggestBox()
         {  
             if(XHR.readyState == 4) //status 4 czyli wszystko wczytane
             { 
-                
                 if (XHR.status == 200) //200 oznacza brak błędu kod 200
                 {
                     XMLMainElement = XHR.responseXML.documentElement; //pobieramy odpowiedz typu xml główny element wojewodztwa(root)
@@ -36,13 +37,57 @@ function suggestBox()
                 }
                 else
                 {
-                    alert("wystąpił błąd" + XHR.status); 
+                  alert("wystąpił błąd" + XHR.status); 
                 }
             }
-            XHR.send(null);
+           
         }
+        XHR.send(null);
     }
 }
 
-  
-  
+function showBox()
+{
+    if(XMLMainElement != null)
+    {
+       document.getElementById("uggestBoxField").style.visibility= "hidden";
+       document.getElementById("uggestBoxField").innerHTML = "";
+
+       var wojewodztwa = XMLMainElement.getElementsByTagName("województwo");
+        
+        if (document.getElementById("hojewodztwo").value != "") // gdy cos wpiszemy w pole to niech sie pojawi jak k=jest nic to nie
+        {
+        for(var i = 0; 1 < wojewodztwa.length; i++)
+           if (wojewodztwa[i].getElementsByTagName("Nazwa")[0].firstChild.nodeValue.toLoverCase().indexOf(document.getElementById("wojewodztwo").value.toLoverCase()) == 0);//pobieramy z tag wojewodztwo  nazwę oraz jej dziecko czyli tekst jak damy node value to wyswietli nam tekst
+           {
+            var suggestBoxField =  document.getElementById("suggestBoxField") //indexof pokazuje nam pozycje tego co wpiszemy do wojewodztwo jezeli wpiszemy p to jak nie ma w "nazwie" to bedzie -1 jak jest na pierwszej pozycji to 0 na 2 to 1 itd
+            suggestBoxField.style.visibility= "visable";
+
+           var tmpDiv = document.createElement("div");
+
+           tmpDiv.className = "podpowiedzi";
+           tmpDiv.onmouseover = function()
+           {
+             this.className = "podpowiedzihover";
+           }
+           tmpDiv.onmouseout = function()
+           {
+             this.className = "podpowiedzi";
+           }
+           tmpDiv.onclick = function()
+           {
+            document.getElementById("wojewodztwo").value = this.innerHTML;
+            suggestBoxField.style.visibility = "hidden";
+           }
+
+           tmpDiv.innerHTML = (wojewodztwa[i].getElementsByTagName("Nazwa")[0].firstChild.nodeValue)
+            
+           suggestBoxField.appendChild(tmpDiv);
+
+           }
+           if(document.getElementById("suggestBoxField").childNodes.lenght == 0); //ilośc podpowiedzi jaka sie pojawi jeżeli 0 to 0jak wiecej to tablica 
+            document.getElementById("wojewodztwo").className = "error";
+        }
+    }
+
+}
