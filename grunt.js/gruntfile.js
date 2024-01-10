ab8ac2f63f28eb7bb8e4a3e2b97e3232150dc488
urszula.krawczyk_007@wp.pl <urszula.krawczyk_007@wp.pl>
2024-01-10 20:03:55 +0100

    adds tasks

--- grunt.js/gruntfile.js
+++ grunt.js/gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt){
+   // require('load-grunt-tasks') (grunt);
     //project configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -11,18 +12,22 @@ module.exports = function(grunt){
                src: 'css/style.css',
                dest: 'css/styleprefixed.css'
               }
-            },
+             },
             watch:
-            {
+             {
                 autoprefixer: //jezeli chcemy obserwowac wiecej niz jeden plik nadajemy etykiete np. autoprefixer
                {
                 files: 'css/style.css',
-                tasks: ['autoprefixer']  //po przecinku mozna dodac task
+                tasks: ['autoprefixer', 'cssmin']  //po przecinku mozna dodac task
+               },
+               jsfilecompresor:
+               {
+                files: 'js/*.js',  //zapis z asterysk wybiera wszytkie pliki js
+                tasks: ['uglify'] 
                }
-
-            },
+             },
             cssmin:
-            {
+             {
                 target:
                 {
                     files:
@@ -30,15 +35,26 @@ module.exports = function(grunt){
                         'css/styleprefixed.min.css': ['css/styleprefixed.css'] // pierwsze nazwa pliku w któym bedzie cssmin w klamrach pliki z których bedzie tworzony css min jezli jest wiwecej niz jeden plik css to je wpsiujemy po przecinku i je połączy w 1.
                     }
                 }
-            }
+             },
+            uglify:
+             {
+                target:
+                {
+                    files:
+                    {
+                        'js/output.min.js': ['js/script.js', 'js/script2.js'] // pierwsze nazwa pliku w któym bedzie cssmin w klamrach pliki z których bedzie tworzony css min jezli jest wiwecej niz jeden plik css to je wpsiujemy po przecinku i je połączy w 1.
+                    }
+                }
+             }
           
     });
-
+    
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-    grunt.registerTask("default", ['autoprefixer', 'watch','cssmin']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask("default", ['autoprefixer', 'watch']);
 
    // grunt.registerTask("nazwaWlasna", "opis tego co sie tu zdarza", ['autoprefixer']);
 
